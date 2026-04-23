0a4f2731f1c9810aece2bbb5ccefcc1ae8fd3d25
pekkarinenaaro <aaro.pekkarinen@student.lab.fi>
2026-04-23 19:19:40 +0300

    Update web.php

--- web.php
+++ web.php
@@ -1,15 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
     return view('etusivu');
 });
+
 Route::get('/lisaa', function () {
     return view('lisaa');
 });
+
 Route::post('/lisaa', function () {
     $nimi = request('name');
+    $kuvaus = request('description');
 
-    return 'Lomake lähetettiin. Kirjoitit nimeksi: ' . $nimi;
+    $rivi = "Nimi: $nimi | Kuvaus: $kuvaus" . PHP_EOL;
+
+    File::append(storage_path('app/ratkaisut.txt'), $rivi);
+
+    return 'Tallennettu tiedostoon';
 });
