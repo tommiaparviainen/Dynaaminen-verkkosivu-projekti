7f483a2b19b873b2742a26c8a8ad157e211b365d
pekkarinenaaro <aaro.pekkarinen@student.lab.fi>
2026-04-24 17:25:33 +0300

    Create Solution.php

---
+++ Solution.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Solution extends Model
+{
+    protected $fillable = ['name', 'description', 'problem_area_id'];
+    public function problemArea()
+{
+    return $this->belongsTo(\App\Models\ProblemArea::class);
+}
+}
