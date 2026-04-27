164e344393925097e8d1e042e84a40f80f061037
pekkarinenaaro <aaro.pekkarinen@student.lab.fi>
2026-04-27 16:03:11 +0300

    Create haku.blade.php

---
+++ haku.blade.php
@@ -0,0 +1,28 @@
+<a href="/">Takaisin etusivulle</a>
+
+<h1>Hae ratkaisuja</h1>
+
+<form method="GET" action="/haku">
+    <div>
+        <label>Ongelma-alue</label><br>
+        <select name="problem_area_id">
+            @foreach ($areas as $area)
+                <option value="{{ $area->id }}">{{ $area->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <br>
+
+    <div>
+        <label>Tagit</label><br>
+        @foreach ($tags as $tag)
+            <input type="checkbox" name="tags[]" value="{{ $tag->id }}">
+            {{ $tag->name }}<br>
+        @endforeach
+    </div>
+
+    <br>
+
+    <button type="submit">Hae</button>
+</form>
